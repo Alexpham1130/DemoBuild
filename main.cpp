ff16aae82f858b16773f2abb8dd5301f3e8406ff
Alex Pham <47752177+Alexpham1130@users.noreply.github.com>
2022-09-14 13:40:05 +0800

    Update test

--- main.cpp
+++ main.cpp
@@ -4,5 +4,6 @@ int main() {
     std::cout << "Hello AWS DevOps! New\n";
     std::cout << "Code Pipeline\n";
     std::cout << "Start build code\n";
+    std::cout << "Demo for Sep 14n";
     return 0;
 }
